a2203b4042e88f6c22e374e8a6234c1407a98534
Grasiele Gomes <grasielegomes13@gmail.com>
2023-08-31 21:11:50 -0300

    atualizando o projeto com responsividade

---
+++ README.md
@@ -0,0 +1,6 @@
+# decodificador
+Projeto para o primeiro Challenge da ONE/ALURA
+A página apresenta um encriptador/desencriptador de mensagens, que irá encriptar/desencriptar todas as vogais escritas.
+
+## 28/08/2023 
+Atualização no design da página.
---
+++ script.js
@@ -0,0 +1,50 @@
+const inputTexto = document.querySelector(".campo-input");
+const outputTexto = document.querySelector(".campo-output");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value);
+    outputTexto.value = textoEncriptado;
+    outputTexto.style.backgroundImage="none";
+    
+    
+
+
+}
+
+function encriptar (stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesincriptado = desencriptar(outputTexto.value);
+    outputTexto.value = textoDesincriptado;
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["ai", "a"], ["enter", "e"], ["imes", "i"], ["ober", "o"], ["ufat", "u"]];
+
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    }
+    return stringDesencriptada;
+}
+
+
+document.getElementById('clipboardcopy').addEventListener('click', clicboarCopy);
+async function clicboarCopy() {
+    let text = document.querySelector(".campo-output").value;
+    await navigator.clipboard.writeText(text);
+    
+}
